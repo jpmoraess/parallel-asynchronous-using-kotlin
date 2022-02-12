6bc49b3629d1e457e363877494379fcd621dacba
João Pedro Moraes <joao.moraes@capela.com.br>
2022-02-12 17:04:01 -0300

    sequential() and parallel() functions in streams API

--- src/main/kotlin/br/com/moraesit/parallelstreams/ParallelStreamsExample.kt
+++ src/main/kotlin/br/com/moraesit/parallelstreams/ParallelStreamsExample.kt
@@ -19,11 +19,25 @@ fun stringTransform(namesList: List<String>): List<String> {
         .collect(Collectors.toList())
 }
 
+fun stringTransform_1(namesList: List<String>, isParallel: Boolean): List<String> {
+
+    val namesStream = namesList.stream()
+
+    if (isParallel)
+        namesStream.parallel()
+
+    return namesStream
+        .map { addNameLengthTransform(it) }
+        .collect(Collectors.toList())
+}
+
 fun stringTransform_parellel(namesList: List<String>): List<String> {
     return namesList
         .stream()
         .parallel()
         .map { addNameLengthTransform(it) }
+        .sequential() // change the entire stream to sequential
+        .parallel()
         .collect(Collectors.toList())
 }
 
--- src/test/kotlin/ParallelStreamsExampleTest.kt
+++ src/test/kotlin/ParallelStreamsExampleTest.kt
@@ -1,4 +1,5 @@
 import br.com.moraesit.parallelstreams.stringTransform
+import br.com.moraesit.parallelstreams.stringTransform_1
 import br.com.moraesit.parallelstreams.stringTransform_parellel
 import br.com.moraesit.util.CommonUtil.Companion.startTimer
 import br.com.moraesit.util.CommonUtil.Companion.timeTaken
@@ -31,6 +32,36 @@ class ParallelStreamsExampleTest {
         //when
         startTimer()
         val resultList = stringTransform_parellel(names)
+        timeTaken()
+
+        //then
+        assertEquals(4, resultList.size)
+        resultList.forEach { assertTrue(it.contains("-")) }
+    }
+
+    @Test
+    fun stringTransform1_true_Test() {
+        //given
+        val names = listOf("João", "Pedro", "Moraes", "Santos")
+
+        //when
+        startTimer()
+        val resultList = stringTransform_1(names, true)
+        timeTaken()
+
+        //then
+        assertEquals(4, resultList.size)
+        resultList.forEach { assertTrue(it.contains("-")) }
+    }
+
+    @Test
+    fun stringTransform1_false_Test() {
+        //given
+        val names = listOf("João", "Pedro", "Moraes", "Santos")
+
+        //when
+        startTimer()
+        val resultList = stringTransform_1(names, false)
         timeTaken()
 
         //then
