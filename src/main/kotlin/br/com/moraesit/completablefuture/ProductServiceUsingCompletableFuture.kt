b8952ba90f00718c878a8538696debd723a041bb
jpmoraess <moraesdeveloperweb@gmail.com>
2022-04-13 18:18:40 -0300

    Integrate InventoryService in ProductService - Approach 1

--- src/main/kotlin/br/com/moraesit/completablefuture/ProductServiceUsingCompletableFuture.kt
+++ src/main/kotlin/br/com/moraesit/completablefuture/ProductServiceUsingCompletableFuture.kt
@@ -1,14 +1,19 @@
 package br.com.moraesit.completablefuture
 
 import br.com.moraesit.domain.Product
+import br.com.moraesit.domain.ProductInfo
+import br.com.moraesit.domain.ProductOption
+import br.com.moraesit.service.InventoryService
 import br.com.moraesit.service.ProductInfoService
 import br.com.moraesit.service.ReviewService
 import br.com.moraesit.util.CommonUtil
 import java.util.concurrent.CompletableFuture
+import java.util.stream.Collectors
 
 class ProductServiceUsingCompletableFuture(
     private val reviewService: ReviewService,
     private val productInfoService: ProductInfoService,
+    private val inventoryService: InventoryService
 ) {
 
     fun retrieveProductDetails(productId: String): Product {
@@ -25,6 +30,33 @@ class ProductServiceUsingCompletableFuture(
         return product
     }
 
+    fun retrieveProductDetailsWithInventory(productId: String): Product {
+        CommonUtil.startTimer()
+
+        val cfProductInfo = CompletableFuture.supplyAsync { productInfoService.retrieveProductInfo(productId) }
+            .thenApply { productInfo ->
+                productInfo.productOptions = updateInventory(productInfo)
+                return@thenApply productInfo
+            }
+        val cfReview = CompletableFuture.supplyAsync { reviewService.retriveReview(productId) }
+
+        val product = cfProductInfo.thenCombine(cfReview) { productInfo, review ->
+            Product(productId, productInfo, review)
+        }.join() // block the thread
+
+        CommonUtil.timeTaken()
+        return product
+    }
+
+    private fun updateInventory(productInfo: ProductInfo): MutableList<ProductOption> {
+        return productInfo.productOptions.stream().map { productOption ->
+            val inventory = inventoryService.retrieveInventory(productOption)
+            productOption.inventory = inventory
+            return@map productOption
+        }
+            .collect(Collectors.toList())
+    }
+
     fun retrieveProductDetails_approach2(productId: String): CompletableFuture<Product> {
         val cfProductInfo = CompletableFuture.supplyAsync { productInfoService.retrieveProductInfo(productId) }
         val cfReview = CompletableFuture.supplyAsync { reviewService.retriveReview(productId) }
@@ -38,8 +70,9 @@ class ProductServiceUsingCompletableFuture(
 fun main(args: Array<String>) {
     val productInfoService = ProductInfoService()
     val reviewService = ReviewService()
-    val productService = ProductServiceUsingCompletableFuture(reviewService, productInfoService)
+    val inventoryService = InventoryService()
+    val productService = ProductServiceUsingCompletableFuture(reviewService, productInfoService, inventoryService)
     val productId = "ABC123"
-    val product = productService.retrieveProductDetails(productId)
+    val product = productService.retrieveProductDetailsWithInventory(productId)
     println(product)
 }
--- src/test/kotlin/br/com/moraesit/completablefuture/ProductServiceUsingCompletableFutureTest.kt
+++ src/test/kotlin/br/com/moraesit/completablefuture/ProductServiceUsingCompletableFutureTest.kt
@@ -1,16 +1,19 @@
 package br.com.moraesit.completablefuture
 
+import br.com.moraesit.service.InventoryService
 import br.com.moraesit.service.ProductInfoService
 import br.com.moraesit.service.ReviewService
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class ProductServiceUsingCompletableFutureTest {
 
     private val reviewService = ReviewService()
     private val productInfoService = ProductInfoService()
-    private val service = ProductServiceUsingCompletableFuture(reviewService, productInfoService)
+    private val inventoryService = InventoryService()
+    private val service = ProductServiceUsingCompletableFuture(reviewService, productInfoService, inventoryService)
 
     @Test
     fun retrieveProductDetails() {
@@ -40,5 +43,23 @@ class ProductServiceUsingCompletableFutureTest {
             assertTrue(it.productInfo!!.productOptions.size > 0)
             assertNotNull(it.review)
         }.join()
+    }
+
+    @Test
+    fun retrieveProductDetailsWithInventory() {
+        // given
+        val productId = "ABC123"
+
+        // when
+        val product = service.retrieveProductDetailsWithInventory(productId)
+
+        // then
+        assertNotNull(product)
+        assertTrue(product.productInfo!!.productOptions.size > 0)
+        product.productInfo!!.productOptions.forEach { productOption ->
+            assertNotNull(productOption.inventory)
+            assertEquals(productOption.inventory!!.count, 2)
+        }
+        assertNotNull(product.review)
     }
 }
