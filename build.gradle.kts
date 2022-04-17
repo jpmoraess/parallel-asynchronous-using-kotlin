3b6a5d20649325b6d7d4fbced4f3080d3c468f67
jpmoraess <moraesdeveloperweb@gmail.com>
2022-04-17 20:54:57 -0300

    Build the RestClient for MoviesService

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,18 @@ repositories {
 
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.0")
+
+    //webclient dependencies
+    implementation("org.springframework:spring-webflux:5.2.9.RELEASE")
+    implementation("io.projectreactor.netty:reactor-netty:0.9.12.RELEASE")
+
+    //jackson dependencies
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.11.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.11.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.11.1")
+
     testImplementation("org.mockito:mockito-core:3.2.4")
     testImplementation("org.mockito:mockito-junit-jupiter:3.2.4")
     testImplementation("org.mockito:mockito-inline:3.2.4")
---
+++ src/main/kotlin/br/com/moraesit/apiclient/MoviesClient.kt
@@ -0,0 +1,48 @@
+package br.com.moraesit.apiclient
+
+import br.com.moraesit.domain.movie.Movie
+import br.com.moraesit.domain.movie.MovieInfo
+import br.com.moraesit.domain.movie.Review
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriComponentsBuilder
+import java.util.stream.Collectors
+
+class MoviesClient(
+    private val webClient: WebClient,
+) {
+
+    fun retrieveMovie(movieInfoId: Long): Movie {
+        // movieInfo
+        val movieInfo = invokeMovieInfoService(movieInfoId)
+
+        // review
+        val review = invokeReviewsService(movieInfoId)
+
+        return Movie(movieInfo, review)
+    }
+
+    private fun invokeMovieInfoService(movieInfoId: Long): MovieInfo {
+        val moviesInfoUrlPath = "/v1/movie_infos/{movieInfoId}"
+        return webClient
+            .get()
+            .uri(moviesInfoUrlPath, movieInfoId)
+            .retrieve()
+            .bodyToMono(MovieInfo::class.java)
+            .block()
+    }
+
+    private fun invokeReviewsService(movieInfoId: Long): List<Review> {
+        val reviewUri = UriComponentsBuilder.fromUriString("/v1/reviews")
+            .queryParam("movieInfoId", movieInfoId)
+            .buildAndExpand()
+            .toUriString()
+
+        return webClient
+            .get()
+            .uri(reviewUri)
+            .retrieve()
+            .bodyToFlux(Review::class.java)
+            .collect(Collectors.toList())
+            .block()
+    }
+}
